aa5257b87c83f5537d28950e7d0fc486ea41c24d
April <apriljoy040370@hotmail.co.uk>
2023-05-20 23:02:18 +0100

    completed without tests

--- src/main/java/DayOne.java
+++ src/main/java/DayOne.java
@@ -3,22 +3,21 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.stream.Stream;
 
 public class DayOne {
-
-
-
-    public int[] splitList(String input) {
+    public ArrayList<Integer> splitList(String input) {
         String fileName = "C:\\Users\\April\\IdeaProjects\\AOC2022\\src\\main\\resources\\dayOneinput.txt";
-int total = 0;
-ArrayList<Integer> Totals = new ArrayList<>();
+        int total = 0;
+        ArrayList<Integer> Totals = new ArrayList<>();
         //read file into stream, try-with-resources
         try (Stream<String> stream = Files.lines(Paths.get(fileName))) {
-
             stream.forEach(item -> {
-                        if (item.equals("\n")) {
-                            Totals.add(Integer.valueOf(total));
+                        if (item.equals("")) {
+                            Totals.add(-1);
+                        } else {
+                            Totals.add(Integer.valueOf(item));
                         }
                     }
             );
@@ -26,6 +25,18 @@ ArrayList<Integer> Totals = new ArrayList<>();
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return new int[]{2};
+        ArrayList<Integer> calories = new ArrayList<>();
+        for (Integer berry : Totals
+        ) {
+            if (berry >= 0) {
+                total = total + berry;
+            } else {
+                calories.add(total);
+                total = 0;
+            }
+        }
+        System.out.println("Maximum Element in ArrayList = "
+                + Collections.max(calories));
+        return calories;
     }
 }
--- src/test/java/dayOneTests.java
+++ src/test/java/dayOneTests.java
@@ -1,12 +1,12 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+
 public class dayOneTests {
     @Test
-    public void convertsToArrays(){
-        DayOne dayone = new DayOne();
-        String input = "10 3 4 \\n\\n 8 7";
-        int[] output = dayone.splitList(input);
-        Assertions.assertArrayEquals(output, new int[]{17, 15});
+    public void convertsToArrays() {
+       DayOne dayOne = new DayOne();
+       dayOne.splitList("hi");
     }
 }
